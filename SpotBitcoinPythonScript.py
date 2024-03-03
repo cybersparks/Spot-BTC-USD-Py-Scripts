d9cb21022c444df8f6137417a1110382e2107139
Jessie Gonzalez <Jessie.Gonzalez@sgs.com>
2024-03-03 14:01:37 -0800

    Adding .py python script

---
+++ SpotBitcoinPythonScript.py
@@ -0,0 +1,17 @@
+#This script gets the current SPOT BTC in USD from Coinbase. 
+#Documentaiton https://docs.cloud.coinbase.com/sign-in-with-coinbase/docs/api-prices
+#YT https://www.youtube.com/watch?v=A9M4BOyoH_w
+
+import requests #Can handle HTTP requests / Can query data from websites
+import json #JSON file parser library
+
+#variable storing coinbase API URL in python Format
+url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot'
+
+#requests.get function, send the http request using url variable
+response = requests.get(url)
+
+#Process with the JSON loads function
+spot_price = json.loads(response.text)
+#show spot btc price
+print(spot_price)

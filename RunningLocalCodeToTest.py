bfb4c8bb1de6e0a929113d45ba46679244b52635
Jessie Gonzalez <Jessie.Gonzalez@sgs.com>
2024-03-18 19:57:07 -0700

    Uploading code for testing

---
+++ RunningLocalCodeToTest.py
@@ -0,0 +1,15 @@
+#Below is a conditional statement with an elif statment.
+jesse = 250
+if jesse == 200:
+    print("Success for a conditional statement")
+elif jesse == 300:
+    print("THIS IS A elif statment")
+#Adding an else statement
+else:
+    print("The else statement worked!")
+#looping through a dictionary
+for i in {"1:Je", "2:Alicia", "3:Maria", "4:Mohammed"}:
+    print("These are the names in a Dictionary",i)
+#looping through a list
+for i in ["Jose", "James", "Josier", "Christoff"]:
+    print ("These are the names on my list",i)

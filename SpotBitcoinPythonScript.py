f69971d249c40bcb666adf6f9ed191c2febcda4c
Jessie Gonzalez <Jessie.Gonzalez@sgs.com>
2024-03-23 17:56:54 -0700

    Adding notes made to code. Added csv write code

--- SpotBitcoinPythonScript.py
+++ SpotBitcoinPythonScript.py
@@ -3,6 +3,7 @@
 
 import requests #Can handle HTTP requests / Can query data from websites
 import json #JSON file parser library
+import csv # importing the csv module
 
 #variable storing coinbase API URL in python Format
 url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot'
@@ -13,8 +14,29 @@ response = requests.get(url)
 #Process with the JSON loads function
 spot_price = json.loads(response.text)
 #show spot btc price
-print(spot_price)
+print(spot_price) ###CODE WORKS UP TO THIS POINT, 3/23/24
 
-**lEAVING THIS HERE, INSTRUCTIONS ON HOW THE CSV MODULE, AND WRITE USING A DICTIONARY TO A CSV
-Source Below
-https://www.geeksforgeeks.org/working-csv-files-python/
+##**lEAVING THIS HERE, INSTRUCTIONS ON HOW THE CSV MODULE, AND WRITE USING A DICTIONARY TO A CSV
+#Source Below
+#https://www.geeksforgeeks.org/working-csv-files-python/
+
+
+# my data rows as dictionary objects
+mydict = spot_price ###THIS VARIABLE NEEDS TO BE FIXED AS A PROPER DICTIONARY. THIS CODE CREATES A CSV. :)
+
+# field names
+fields = ['amount', 'base', 'year', 'currency']
+
+# name of csv file
+filename = "current_spot_price_btc.csv"
+
+# writing to csv file
+with open(filename, 'w') as csvfile:
+	# creating a csv dict writer object
+	writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+	# writing headers (field names)
+	writer.writeheader()
+
+	# writing data rows
+	writer.writerows(mydict)
